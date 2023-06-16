66e68037f6eb764d705b06484ecc6f89d457fab6
Lucas Sandin <lucasfernandessandin@gmail.com>
2023-06-16 17:20:16 -0300

    update

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -5,8 +5,6 @@ import { useNavigateContext } from "../../Context/NavigateContext"
 import Footer from "../../Components/Footer";
 import Content from "../../Components/Content";
 
-
-import casal from "../../Assets/casal-tela-inicial.png";
 import * as S from '../../Components/styles/styles';
 
 export default function Home() {
@@ -22,13 +20,7 @@ export default function Home() {
       {/* central content */}
       <Content>
         <S.Row>
-          <S.Casal src={casal} alt="Casal" loading="lazy"/>
-        </S.Row>
-        <S.Row>
           <S.Title>Vídeo Chamada</S.Title>
-        </S.Row>
-        <S.Row>
-          <p>Lorem Ipsum is simply dummy text of.....</p>
         </S.Row>
       </Content>
 
@@ -36,10 +28,7 @@ export default function Home() {
       <Footer>
         <S.Row>
           <S.Column>
-            <S.ButtonSecondary onClick={() => navigate("/atendimento/orientacoes")}>Já agendei</S.ButtonSecondary>
-          </S.Column>
-          <S.Column>
-            <S.ButtonPrimary fullwidth={"true"} onClick={() => navigate("/agendamento/titular-beneficiario")}>Primeiro acesso</S.ButtonPrimary>
+            <S.ButtonPrimary fullwidth={"true"}>LOGIN</S.ButtonPrimary>
           </S.Column>
         </S.Row>
       </Footer>
