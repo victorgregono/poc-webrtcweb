905f1334abdfe83585bbd6fffaaa4b9ab45fdeaa
victorgrego <victorgrego_2@hotmail.com>
2023-06-20 17:25:55 -0300

    melhoria tela login

--- src/Pages/Login/styles.js
+++ src/Pages/Login/styles.js
@@ -1,29 +1,30 @@
 import { styled } from "styled-components";
 
-// export const Location = styled.div`
-//   display: inherit;
-//   justify-content: center;
-//   margin: 5rem 0 2rem;
-// `;
+import LoginFundo from "../../Assets/LoginFundo.svg";
 
 export const Location = styled.div`
-  
-  background-color: #1526ff;
-  
-`;
+  background-image: url(${LoginFundo});
 
-export const img = styled.img`
-  width: 100px;
-  height: 100px;
-  margin: auto; /* Centraliza horizontalmente */
-  display: block; /* Remove espaço extra abaixo da imagem */
+  position: absolute;
+  top: 0px;
+  right: 0px;
+  bottom: 0px;
+  left: 0px;
 `;
 
 export const Login = styled.div`
   background-color: #ffffff;
-  width: 500px;
-  height: 600px;
+  position: absolute;
+  top: 0px;
+  right: 0px;
+  bottom: 30px;
+  left: 0px;
+  width: 400px;
+  height: 426px;
   margin: auto;
+  box-shadow: 0px 0px 10px black;
+  padding: 20px;
+  box-sizing: border-box;
 `;
 
 export const Title = styled.span`
@@ -34,21 +35,44 @@ export const Title = styled.span`
 
   color: #ffffff;
   margin-top: 1rem;
+
+  display: block;
 `;
 
-export const P = styled.p`
+export const PP = styled.p`
+  color: #ffffff;
+  text-align: right;
+`;
 
-  `;
+export const Inputt = styled.input`
+  border: 2px solid #000000;
+  border-radius: 4px;
+  height: 70px;
+  padding-left: 1rem;
 
+  &::-webkit-input-placeholder {
+    /* Edge */
+    font-style: normal;
+    font-weight: 700;
+    font-size: 18px;
+    line-height: 21px;
+    color: #000000;
+  }
 
+  &:-ms-input-placeholder {
+    /* Internet Explorer 10-11 */
+    font-style: normal;
+    font-weight: 700;
+    font-size: 18px;
+    line-height: 21px;
+    color: #000000;
+  }
 
-
-// export const Login = styled.div`
-//   background-color: #ffffff;
-//   width: 500px;
-//   height: 600px;
-//   margin: auto;
-// `;
-
-// text-align: center;
-//   color: #1526FF;
+  &::placeholder {
+    font-style: normal;
+    font-weight: 700;
+    font-size: 18px;
+    line-height: 21px;
+    color: #000000;
+  }
+`;
